1843b3df3f04062f8fa8a67d9aad1372a961bc2b
philbeaudry <philippe.beaudry96@gmail.com>
2018-04-08 15:04:23 -0400

    db config file to make it easy to connect when we get to school

--- company/fancy/connection.php
+++ company/fancy/connection.php
@@ -1,6 +1,8 @@
 <?php
 
-$con = new mysqli('localhost', 'root', '', 'company', 3306, '')
+$config = include('config.php');
+
+$con = new mysqli($config['host'], $config['user'], $config['password'], $config['dbname'], $config['port'], $config['socket'])
     or die ('Could not connect to the database server'.mysqli_connect_error());
 
 return $con // REMEMBER TO CLOSE CONNECTIONS
